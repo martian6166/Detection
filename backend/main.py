d9b0724005ad7ca345198616f70fd789de84e0f5
nathanieluriri <uririnathaniel@gmail.com>
2025-03-07 06:09:00 +0100

    disease stuff

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ def home():
 def login(user:UserBody):
     user ={"username":user.username,"password":user.password}
     print(user)
-    user_id,username= login_user(db_uri=MONGO_URI,db_name="crayonics",collection_name="users",document=user)
+    user_id,username= login_user(db_uri=MONGO_URI,db_name="diseaseDetector",collection_name="users",document=user)
     
     if user_id != False:
         return {"user_id":user_id,"message":f"welcome back {username}"}
@@ -33,7 +33,7 @@ def login(user:UserBody):
 @app.post("/auth/signup",tags=["Authentication"])
 def signUp(user:UserBody):
     user ={"username":user.username,"password":user.password,"first_name":user.firstName,"last_name":user.lastName}
-    user_id= create_user(db_uri=MONGO_URI,db_name="crayonics",collection_name="users",document=user)
+    user_id= create_user(db_uri=MONGO_URI,db_name="diseaseDetector",collection_name="users",document=user)
     if user_id != False:
         return {"user_id":user_id,"message":"User created Successfully"}
     return JSONResponse(status_code=status.HTTP_226_IM_USED,content="user already Exists")
